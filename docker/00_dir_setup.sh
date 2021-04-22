3acf3e363a68398a09411c7458de2b9cb5c1960b
t-tibor <tusori.tibor94@gmail.com>
2021-04-23 00:01:13 +0200

    Use docker volumes for data storage instead of bind mounts. Remove absolue path usage. Remove .env file.

--- docker/00_dir_setup.sh
+++ docker/00_dir_setup.sh
@@ -8,5 +8,6 @@ mkdir -p $TEMP_LOGGER_HOME/docker/compose-files/influxdb
 
 
 mkdir -p $TEMP_LOGGER_HOME/docker/grafana/data
-mkdir -p $TEMP_LOGGER_HOME/docker/grafana/provisioning
+sudo chown 472:472 $TEMP_LOGGER_HOME/docker/grafana/data
+kdir -p $TEMP_LOGGER_HOME/docker/grafana/provisioning
 mkdir -p $TEMP_LOGGER_HOME/docker/compose-files/grafana
